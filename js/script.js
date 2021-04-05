295e67c6a73542ce1b514fb719ae47ca3d1b0c30
AndreiHome <uzatsepin@gmail.com>
2021-04-05 12:25:49 +0300

    fetch api, promises

--- js/script.js
+++ js/script.js
@@ -235,9 +235,6 @@ window.addEventListener('DOMContentLoaded', () => {
             
             form.insertAdjacentElement('afterend', statusMessage);
 
-            const request = new XMLHttpRequest();
-            request.open('POST', 'server.php');
-            request.setRequestHeader('Content-type', 'application/json');
             const formData = new FormData(form);
 
             const object = {};
@@ -246,18 +243,22 @@ window.addEventListener('DOMContentLoaded', () => {
                 object[key] = value;
             });
 
-            const json = JSON.stringify(object);
-
-            request.send(json);
-            request.addEventListener('load', () => {
-                if (request.status === 200) {
-                    console.log(request.response);
-                    showThingsModal(message.success);
-                    form.reset();
-                    statusMessage.remove();
-                } else {
-                    showThingsModal(message.failure);
-                }
+            fetch('server.php', {
+                method: "POST",
+                headers: {
+                    'Content-type': 'application/json'
+                },
+                body: JSON.stringify(object)
+            })
+            .then(data => data.text())
+            .then(data => {
+                console.log(data);
+                showThingsModal(message.success);
+                statusMessage.remove();
+            }).catch(() => {
+                showThingsModal(message.failure);
+            }).finally(() => {
+                form.reset();
             })
         });
     }
@@ -284,5 +285,15 @@ window.addEventListener('DOMContentLoaded', () => {
             closeModal();
         }, 4000)
     }
+
+    fetch('https://jsonplaceholder.typicode.com/posts', {
+        method: 'POST',
+        body: JSON.stringify({name: 'Alex'}),
+        headers: {
+            'Content-type': 'application/json'
+        }
+    })
+        .then(response => response.json())
+        .then(json => console.log(json));
 
 });
